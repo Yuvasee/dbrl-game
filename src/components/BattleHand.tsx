f5444d491d5c75477c2cc31d41b04be23d6919d9
Yuri Vasilchikov <yuri.vasilchikov@clarizen.com>
2021-06-20 21:59:49 +0300

    Player hand styles changed

--- src/components/BattleHand.tsx
+++ src/components/BattleHand.tsx
@@ -8,12 +8,13 @@ import { State } from "state";
 import { CardPaper, CARD_HEIGHT, CARD_WIDTH } from "./CardPaper";
 import "./BattleHand.css";
 
+const CARDS_FAN_DEGREE = 20;
+
 const Wrapper = styled(Card)<never, { cardsAmount: number }>({
     height: CARD_HEIGHT,
     width: "80%",
-    maxWidth: ({ cardsAmount }) =>
-        `min(${cardsAmount * CARD_WIDTH * 0.6}px, 50%)`,
-    bottom: 0,
+    maxWidth: ({ cardsAmount }) => `min(${cardsAmount * CARD_WIDTH * 0.6}px, 50%)`,
+    bottom: 20,
     left: "50%",
     transform: "translateX(-50%)",
     position: "absolute",
@@ -32,10 +33,7 @@ export const BattleHand = observer(() => {
     const wrapperRef = useRef<HTMLDivElement>(null);
     const [width, setWidth] = useState(0);
 
-    const doMeasure = useCallback(
-        () => setWidth(wrapperRef.current?.clientWidth || 0),
-        []
-    );
+    const doMeasure = useCallback(() => setWidth(wrapperRef.current?.clientWidth || 0), []);
 
     useEffect(() => {
         doMeasure();
@@ -44,21 +42,36 @@ export const BattleHand = observer(() => {
 
     const cards = State.battle!.player.deck.handIds;
 
+    const degToRad = (deg: number) => (Math.PI / 180) * deg;
+
     return (
         <Wrapper elevation={0} ref={wrapperRef} cardsAmount={cards.length}>
             <PositionContainer>
-                {cards.map((cardId, i) => (
-                    <CardContainer
-                        key={cardId}
-                        style={{
-                            left:
-                                i * ((width - CARD_WIDTH) / (cards.length - 1)),
-                        }}
-                        className="hand-card-container"
-                    >
-                        <CardPaper cardId={cardId} />
-                    </CardContainer>
-                ))}
+                {cards.map((cardId, i) => {
+                    const left = i * ((width - CARD_WIDTH) / (cards.length - 1));
+
+                    const rotateDegZ =
+                        (CARDS_FAN_DEGREE / (cards.length - 1)) * i - CARDS_FAN_DEGREE / 2;
+
+                    const verticalFix =
+                        (Math.cos(degToRad(rotateDegZ)) -
+                            Math.cos(degToRad(CARDS_FAN_DEGREE / 2))) *
+                        -(CARD_WIDTH * 7);
+
+                    return (
+                        <CardContainer
+                            key={cardId}
+                            style={{
+                                left,
+                                transform: `rotateZ(${rotateDegZ}deg)`,
+                                top: verticalFix,
+                            }}
+                            className="hand-card-container"
+                        >
+                            <CardPaper cardId={cardId} />
+                        </CardContainer>
+                    );
+                })}
             </PositionContainer>
         </Wrapper>
     );
